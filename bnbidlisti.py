1e82251d7ebdecdba9b4cdad5c9af673383d38f9
hlynurstef <hlynurs15@ru.is>
2016-05-25 23:39:24 +0000

    make script compatible with python 3.5

--- bnbidlisti.py
+++ bnbidlisti.py
@@ -5,74 +5,75 @@ from datetime import datetime
 import requests
 from bs4 import BeautifulSoup
 import re
+from getpass import getpass
 
-with requests.session() as c:
-	# Place your username and password for bn.is here
-	USERNAME = input("Username: ")
-	PASSWORD = input("Password: ")
-	
+logged_in = False
 
-	login_url = "http://www.bn.is/minar-sidur/innskraning/"
-	bidlisti_url = "http://www.bn.is/minar-sidur/stada-a-bidlista/"
+while not logged_in:
+	with requests.session() as c:
+		# Place your username and password for bn.is here
+		USERNAME = input("Username: ")
+		PASSWORD = getpass(prompt="Password: ", stream=None)
 
-	# Logging into bn.is
-	login_data = dict(username=USERNAME, 
-					  password=PASSWORD, 
-					  action="login")
-	c.post(login_url, 
-		   data=login_data, 
-		   headers={"Referer": "http://www.bn.is/"})
+		login_url = "http://www.bn.is/minar-sidur/innskraning/"
+		bidlisti_url = "http://www.bn.is/minar-sidur/stada-a-bidlista/"
 
-	# Use BeautifulSoup to find the correct part 
-	# of the page and put it in a list
-	page = c.get(bidlisti_url)
-	soup = BeautifulSoup(page.content, "html.parser")
-	info_list = soup.find_all("span")
-	list_length = len(info_list)
+		# Logging into bn.is
+		login_data = dict(username=USERNAME, 
+						  password=PASSWORD, 
+						  action="login")
+		c.post(login_url, 
+			   data=login_data, 
+			   headers={"Referer": "http://www.bn.is/"})
 
-	# Regular expressions to find the name of the 
-	# location and number in the waiting list
-	location_re = re.compile("(Reykjavík|Hafnarfjörður)" + 
-							 "[a-zA-Z ðæíúÞ()]*")
-	number_re = re.compile("[0-9]+")
+		# Use BeautifulSoup to find the correct part 
+		# of the page and put it in a list
+		page = c.get(bidlisti_url)
+		soup = BeautifulSoup(page.content, "html.parser")
+		info_list = soup.find_all("span")
+		list_length = len(info_list)
 
-	# Search by string "Númer" with unicode characters
-	search_string = "Númer"
+		# Regular expressions to find the name of the 
+		# location and number in the waiting list
+		location_re = re.compile("(Reykjavík|Hafnarfjörður)" + 
+								 "[a-zA-Z ðæíúÞ()]*")
+		number_re = re.compile("[0-9]+")
 
-	# Open file bn.txt to append, creates the file if it doesn't exist
-	with open("bn.txt", "a+") as myfile:
+		# Search by string "Númer" with unicode characters
+		search_string = "Númer"
 
-		now = datetime.now()
-		date = "Date: %s/%s/%s" % (now.day, now.month, now.year)
-		time = "Time: %s:%s:%s" % (now.hour, now.minute, now.second)
-		print(date)
-		print(time)
-		myfile.write(date + "\n")
-		myfile.write(time + "\n")
-	
-		# For loop that runs regex to find the info we want and prints 
-		# out the results to console and to file
-		for i in range(1, list_length-1):
-			current_item = info_list[i].getText()
-			if search_string in current_item:
+		# Open file bn.txt to append, creates the file if it doesn't exist
+		with open("bn.txt", "a+") as myfile:
 
-				# Print to console
-				location = re.search(location_re, current_item).group()
-				number = re.search(number_re, current_item).group()
-				print(location)
-				print(number)
+			now = datetime.now()
+			date = "Date: %s/%s/%s" % (now.day, now.month, now.year)
+			time = "Time: %s:%s:%s" % (now.hour, now.minute, now.second)
+			print(date)
+			print(time)
+			myfile.write(date + "\n")
+			myfile.write(time + "\n")
 
-				# Print to file
-				myfile.write(location + "\n")
-				myfile.write(number + "\n")
-			else:
+			# For loop that runs regex to find the info we want and prints 
+			# out the results to console and to file
+			for i in range(1, list_length-1):
+				current_item = info_list[i].getText()
+				if search_string in current_item:
+					logged_in = True
+					
+					# Print to console
+					location = re.search(location_re, current_item).group()
+					number = re.search(number_re, current_item).group()
+					print(location)
+					print(number)
 
-				# Nothing was found, possibly due to 
-				# incorrect username/password
-				not_found = ("Not found - make sure you entered " + 
-							 "username/password correctly")
-				print(not_found)
-				myfile.write(not_found + "\n")
-				break
-
-		myfile.write("\n")
+					# Print to file
+					myfile.write(location + "\n")
+					myfile.write(number + "\n")
+					
+				else:
+					# Nothing was found, possibly due to 
+					# incorrect username/password
+					print("Not found - make sure you entered " + 
+								 "username/password correctly")
+					break
+			myfile.write("\n")
